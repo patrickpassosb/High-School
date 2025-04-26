9acbea97cedbb0d9ceb0979a76ff11f0f66bf6f7
patrickpassosb <patrickcuco2@gmail.com>
2025-04-26 11:09:12 -0300

    Update 03_create_indexes.sql

--- SQL/database_jogoDB/03_create_indexes.sql
+++ SQL/database_jogoDB/03_create_indexes.sql
@@ -1,12 +1,34 @@
 -- Index to speed up JOINs on Personagem.JogadorID
-CREATE INDEX IDX_Personagem_JogadorID ON Personagem(JogadorID);
+IF NOT EXISTS (SELECT name FROM sys.indexes WHERE name = 'IDX_Personagem_JogadorID')
+BEGIN
+    CREATE INDEX IDX_Personagem_JogadorID ON Personagem(JogadorID);
+END
+GO
 
--- Index to speed up joins/queries on Inventario.PersonagemID
-CREATE INDEX IDX_Inventario_PersonagemID ON Inventario(PersonagemID);
+-- Index to speed up JOINs/queries on Inventario.PersonagemID
+IF NOT EXISTS (SELECT name FROM sys.indexes WHERE name = 'IDX_Inventario_PersonagemID')
+BEGIN
+    CREATE INDEX IDX_Inventario_PersonagemID ON Inventario(PersonagemID);
+END
+GO
 
--- Index to speed up joins/queries on Inventario.ItemID
-CREATE INDEX IDX_Inventario_ItemID ON Inventario(ItemID);
+-- Index to speed up JOINs/queries on Inventario.ItemID
+IF NOT EXISTS (SELECT name FROM sys.indexes WHERE name = 'IDX_Inventario_ItemID')
+BEGIN
+    CREATE INDEX IDX_Inventario_ItemID ON Inventario(ItemID);
+END
+GO
 
--- Index to help performance on PersonagemMissao
-CREATE INDEX IDX_PersonagemMissao_PersonagemID ON PersonagemMissao(PersonagemID);
-CREATE INDEX IDX_PersonagemMissao_MissaoID ON PersonagemMissao(MissaoID);
+-- Index to help performance on LinguagemMissao.PersonagemID
+IF NOT EXISTS (SELECT name FROM sys.indexes WHERE name = 'IDX_LinguagemMissao_Linguagem')
+BEGIN
+    CREATE INDEX IDX_LinguagemMissao_Linguagem ON LinguagemMissao(Linguagem);
+END
+GO
+
+-- Index to help performance on LinguagemMissao.MissaoID
+IF NOT EXISTS (SELECT name FROM sys.indexes WHERE name = 'IDX_LinguagemMissao_MissaoID')
+BEGIN
+    CREATE INDEX IDX_LinguagemMissao_MissaoID ON LinguagemMissao(MissaoID);
+END
+GO
