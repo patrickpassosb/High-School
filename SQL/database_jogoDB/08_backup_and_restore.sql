e7c308c41879f5218cefa95448b520d1c942db63
patrickpassosb <patrickcuco2@gmail.com>
2025-04-26 11:34:58 -0300

    Update 08_backup_and_restore.sql

--- SQL/database_jogoDB/08_backup_and_restore.sql
+++ SQL/database_jogoDB/08_backup_and_restore.sql
@@ -1,6 +1,6 @@
 -- Script para backup do banco de dados (execute como admin)
 BACKUP DATABASE JogoOnlineDB
-TO DISK = 'C:\Backups\JogoOnlineDB.bak'
+TO DISK = 'C:\SQLBackups\JogoOnlineDB.bak'
 WITH FORMAT,
      NAME = 'Backup Completo do JogoOnlineDB';
 GO
@@ -8,8 +8,8 @@ GO
 -- Script para restaurar o banco de dados (execute como admin)
 -- Certifique-se de que o banco atual esteja desconectado antes
 RESTORE DATABASE JogoOnlineDB_Restaurado
-FROM DISK = 'C:\Backups\JogoOnlineDB.bak'
-WITH MOVE 'JogoOnlineDB' TO 'C:\SQLData\JogoOnlineDB_Restaurado.mdf',
-     MOVE 'JogoOnlineDB_log' TO 'C:\SQLData\JogoOnlineDB_Restaurado_log.ldf',
+FROM DISK = 'C:\SQLBackups\JogoOnlineDB.bak'
+WITH MOVE 'JogoOnlineDB' TO 'C:\SQLBackups\JogoOnlineDB_Restaurado.mdf',
+     MOVE 'JogoOnlineDB_log' TO 'C:\SQLBackups\JogoOnlineDB_Restaurado_log.ldf',
      REPLACE;
 GO
