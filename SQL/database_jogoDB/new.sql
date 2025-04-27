61e2acb15005b154c3d97276d22d2fad58af4bb2
patrickpassosb <patrickcuco2@gmail.com>
2025-04-26 21:27:25 -0300

    Update new.sql

--- SQL/database_jogoDB/new.sql
+++ SQL/database_jogoDB/new.sql
@@ -474,3 +474,20 @@ GO
 GRANT SELECT ON player TO game_reader;
 GRANT SELECT ON mission TO game_reader;
 GO
+
+CREATE VIEW vw_character_full_info AS
+SELECT 
+    c.character_id,
+    p.name AS player_name,
+    c.name AS character_name,
+    cl.name AS class_name,
+    l.name AS language_name,
+    c.level,
+    c.experience
+FROM 
+    character c
+JOIN player p ON c.player_id = p.player_id
+JOIN class cl ON c.class_id = cl.class_id
+JOIN language l ON c.language_id = l.language_id;
+GO
+
