bd99eef8db606c0b854b144652959129b992969d
patrickpassosb <patrickcuco2@gmail.com>
2025-06-20 13:23:13 -0300

    Delete SQL/database_jogoDB directory

--- SQL/database_jogoDB/05_create_functions.sql
+++
@@ -1,7 +0,0 @@
-CREATE FUNCTION CalcularXPProximoNivel (@NivelAtual INT)
-RETURNS INT
-AS
-BEGIN
-    RETURN (@NivelAtual * 300);  -- Regra: precisa de 300 XP vezes o nível atual
-END;
-GO
--- SQL/database_jogoDB/07_user_permissions.sql
+++
@@ -1,9 +0,0 @@
--- (execute esses comandos como admin no SQL Server Management Studio)
-CREATE LOGIN LeitorDoJogo WITH PASSWORD = 'SenhaSegura123!';
-GO
-CREATE USER LeitorDoJogo FOR LOGIN LeitorDoJogo;
-GO
-GRANT SELECT ON Personagem TO LeitorDoJogo;
-GRANT SELECT ON Missao TO LeitorDoJogo;
--- Este usuário só pode visualizar dados nessas tabelas
-GO
